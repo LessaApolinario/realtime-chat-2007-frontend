d1fed510b4deee76c2554d01d6734c96a9e1a642
Lessa Apolinario <lessaapolinario@gmail.com>
2025-08-24 20:19:11 -0300

    Translate home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,20 @@ export default function Home() {
         className="absolute top-0 right-0 bottom-0 left-0 h-screen w-screen"
         src={homeBackground}
         width={4000}
-        alt="Home background"
+        alt="Fundo abstrato da página inicial"
       />
 
       <div className="absolute top-1/2 bottom-1/2 left-8">
         <h1 className="mb-1.5 text-4xl text-zinc-200">Real-time chat 2007</h1>
         <p className="mb-2.5 text-2xl text-zinc-200">
-          Chat with people around the world and make new friends
+          Converse com pessoas ao redor do mundo e faça novos amigos
         </p>
 
         <Link
           href="/auth/login"
           className="inline-block cursor-pointer rounded-md bg-cyan-700 p-4 text-center text-zinc-200"
         >
-          Get started
+          Começar
         </Link>
       </div>
     </main>
