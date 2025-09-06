6d7ac314d6650e8a69f712f898a9044679a80e16
Lessa Apolinario <lessaapolinario@gmail.com>
2025-09-06 08:58:05 -0300

    Disconnect suser ocket when leave chat room

--- src/ui/frontend/hooks/useChatRoom.ts
+++ src/ui/frontend/hooks/useChatRoom.ts
@@ -57,6 +57,7 @@ export function useChatRoom({ roomId, token }: ChatRoomHookProps) {
 
     return () => {
       emitEvent("leaveChatRoom", { roomId });
+      socket.disconnect();
     };
   }, [socket]);
 
